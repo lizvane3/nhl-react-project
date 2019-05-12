2ed2c83e0279f031d1557fccc86002d0bb4a928d
Liz Camacho <lizvane3@gmail.com>
2019-05-12 00:26:39 -0500

    build added

---
+++ build/precache-manifest.42b46a51aebba730893858a05ec1382d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2720483c098195fcf0904eca9a87bac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5fea8d26d4a0e5a73d",
+    "url": "/static/css/main.4126c146.chunk.css"
+  },
+  {
+    "revision": "d05e35acf281230d400b",
+    "url": "/static/js/2.ddca19dd.chunk.js"
+  },
+  {
+    "revision": "6c5fea8d26d4a0e5a73d",
+    "url": "/static/js/main.89ba1ad2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a7d262e72c92d640bff8ef79972f27e",
+    "url": "/static/media/nhllogo.9a7d262e.png"
+  },
+  {
+    "revision": "47ac2c912a55e98e805d1bdec21254b4",
+    "url": "/static/media/nhlwallpaper.47ac2c91.jpg"
+  }
+]);
